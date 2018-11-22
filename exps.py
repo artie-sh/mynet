2c19e52adff2e7bba6a791b1907eade6a29edad5
artie-sh <artie.sh.87@gmail.com>
2018-11-23 00:35:22 +0100

    working

--- exps.py
+++ exps.py
@@ -1,6 +1,5 @@
 import numpy as np
-from mnist_loader import load_data, load_data_wrapper
-import matplotlib.pyplot as plt
+from mnist_loader import load_data
 import matplotlib.image as img
 import random
 import math
@@ -16,37 +15,82 @@ def save_as_png(dataset, number):
 
 
 def init_weights(number):
-    return [random.random() for i in range(number)]
+    return [0 for i in range(number)]
 
 def init_b():
     return random.random()
 
-def calc_z(w, x, b):
-    sum = []
-    assert len(w) == len(x)
-    for i in range(len(w)):
-        #print '%s * %s + %s = %s' % (str(w[i]), str(x[i]), str(b), str(w[i]*x[i]+b))
-        sum.append(w[i]*x[i] + b)
-        #print sum
-    return sum
+def calc_z(W, X, b):
+    sum = 0
+    assert len(W) == len(X)
+    for i in range(len(W)):
+        sum += W[i] * X[i]
+    return sum + b
 
 def calc_sigmoid(z):
-    return [1/(1+math.exp(-i)) for i in z]
+    return 1/(1+math.exp(-z))
 
-def calc_cost(y, a):
+def calc_cost(Y, A):
     sum = 0
-    for i in range(len(a)):
-        sum += y*math.log(a[i]) + (1-y)*math.log(1-a[i])
-        #print '%s - %s' % (str(y), str(math.log(a[i])))
-    return -sum/len(a)
+    assert len(Y) == len(A)
+    for i in range(len(A)):
+       sum += Y[i] * math.log(A[i]) + (1 - Y[i]) * math.log(1 - A[i])
+    return -sum/len(A)
+
+def calc_loss(Y, A):
+    - Y * math.log(A) - (1 - Y) * math.log(1 - A)
+
+def normalize_val(val):
+    result = 0
+    if val == 4:
+        result = 1
+    return result
 
 
-w = init_weights(784)
+def calc_dw(X, A, Y):
+    dws = []
+    A_Y = [A[i] - Y[i] for i in range(len(A))]
 
-b = init_b()
-x = training_img[2]
-y = training_vals[2]
+    for i in range(len(X[0])):
+        sum = 0
+        for j in range(len(A_Y)):
+            sum += X[j][i] * A_Y[j]
+        dws.append(sum/len(X))
+    return dws
 
-predictions = calc_sigmoid(calc_z(w, x, b))
-cost = calc_cost(y, predictions)
-print cost
+def calc_db(Y, A):
+    sum = 0
+    for i in range(len(Y)):
+        sum += A[i] - Y[i]
+    return sum/len(Y)
+
+
+def propagate(W, b, X, Y):
+    A = [calc_sigmoid(calc_z(W, X[i], b)) for i in range(rng)]
+    cost = calc_cost(Y, A)
+    # grads = {'dw': calc_dw(X, A, Y), 'db': calc_db(Y, A)}
+    dw = calc_dw(X, A, Y)
+    db = calc_db(Y, A)
+    return dw, db, cost
+
+
+def optimize(W, b, X, Y):
+    num_iterations = 100
+    learning_rate = 0.1
+
+    for i in range(num_iterations):
+        dw, db, cost = propagate(W, b, X, Y)
+        for j in range(len(W)):
+            W[j] -= learning_rate * dw[j]
+        db -= learning_rate * db
+    return cost
+
+rng = 100
+
+W = init_weights(784)
+b = 0
+X = [training_img[i] for i in range(rng)]
+Y = [normalize_val(training_vals[i]) for i in range(rng)]
+
+print optimize(W, b, X, Y)
+
