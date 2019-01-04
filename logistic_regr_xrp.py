45bbbd994bffb9f1ee010c25e5cafd26bb2d9bd9
artie-sh <artie.sh.87@gmail.com>
2019-01-04 01:48:01 +0100

    preparing ma

--- logistic_regr_xrp.py
+++ logistic_regr_xrp.py
@@ -7,5 +7,27 @@ cur = conn.cursor()
 
 data = csv.read_db(cur, 'xrp')
 
-for i in range(0, 10):
-    print data[i]
+def avg(timerow):
+    res = [line[1] for line in timerow]
+    return sum(res)/max(len(timerow), 1)
+
+def get_timerow(data, rowname='close'):
+    rows = {'open': 1, 'high': 2, 'low': 3, 'close': 4}
+    return [(line[0], line[rows[rowname]]) for line in data[:10]]
+
+def get_ma(timerow, periods):
+    result = []
+    for i in range(len(timerow) - periods):
+        result.append(timerow[i + periods])
+
+    return result
+
+    return [(timerow[i + periods][0], avg([timerow[j][1] for j in range(periods)])) for i in range(len(timerow) - periods)]
+
+
+print get_ma(get_timerow(data, 'close'), 3)
+
+#row = get_timerow(data)
+
+#av = avg(row)
+#print av
