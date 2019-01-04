719f09a91d93eee7b5405ec019b3375712963dbc
artie-sh-weather <artem.shendrikov.contractor@weather.com>
2019-01-04 17:51:34 +0100

    xrp learning done

--- logistic_regr_xrp.py
+++ logistic_regr_xrp.py
@@ -1,4 +1,5 @@
 from database_writer import CsvReader
+from vectorization_working import *
 
 csv = CsvReader()
 
@@ -8,26 +9,45 @@ cur = conn.cursor()
 data = csv.read_db(cur, 'xrp')
 
 def avg(timerow):
-    res = [line[1] for line in timerow]
-    return sum(res)/max(len(timerow), 1)
+    return sum([line[1] for line in timerow])/max(len(timerow), 1)
 
 def get_timerow(data, rowname='close'):
     rows = {'open': 1, 'high': 2, 'low': 3, 'close': 4}
-    return [(line[0], line[rows[rowname]]) for line in data[:10]]
+    colnum = rows[rowname]
+    return [(line[0], line[colnum]) for line in data]
 
-def get_ma(timerow, periods):
-    result = []
-    for i in range(len(timerow) - periods):
-        result.append(timerow[i + periods])
+def get_ma(datarow, periods):
+    return [(datarow[i + periods - 1][0], avg(datarow[i:i + periods])) for i in range(len(datarow) - periods + 1)]
 
-    return result
+def get_min_max(timerow):
+    minimum, maximum = timerow[0][1], timerow[0][1]
+    for line in timerow:
+        if line[1] < minimum:
+            minimum = line[1]
+        if line[1] > maximum:
+            maximum = line[1]
+    return minimum, maximum
 
-    return [(timerow[i + periods][0], avg([timerow[j][1] for j in range(periods)])) for i in range(len(timerow) - periods)]
+def normalize_vals(data, min, max):
+    return (data - min) / (max - min)
+
+start = track_start()
+
+timerow = get_timerow(data)
+ma = get_ma(timerow, 3)
+minimum, maximum = get_min_max(timerow)
+
+input_size = 100
+trainig_sets = 1000
+num_iterations = 5000
+learning_rate = 0.2
+
+W = init_weights(input_size)
+b = 0
+X = normalize_vals(np.array([[item[1] for item in piece] for piece in [ma[i:i+input_size] for i in range(trainig_sets)]]), minimum, maximum)
+Y = normalize_vals(np.array([ma[i+input_size+1][1] for i in range(trainig_sets)]),  minimum, maximum)
+
+W, b = optimize(W, b, X, Y, learning_rate, num_iterations)
 
 
-print get_ma(get_timerow(data, 'close'), 3)
-
-#row = get_timerow(data)
-
-#av = avg(row)
-#print av
+track_end(start)
--- vectorization_working.py
+++ vectorization_working.py
@@ -1,8 +1,6 @@
 import numpy as np
 from mnist_loader import load_data
 import matplotlib.image as img
-import random
-import math
 from datetime import datetime as dt
 from datetime import timedelta
 import params
@@ -107,49 +105,37 @@ trainig_sets = params.trainig_sets
 num_iterations = params.num_iterations
 learning_rate = params.learning_rate
 
-W = init_weights(784)
-b = 0
-X = np.array([training_img[i] for i in range(trainig_sets)])
-Y = np.array([normalize_val(training_vals[i], target_number) for i in range(trainig_sets)])
-
-W, b = optimize(W, b, X, Y, learning_rate, num_iterations)
-
-true_rec, false_rec, true_unrec, false_unrec = 0, 0, 0, 0
-
-result = predict(W, [training_img[i] for i in range(400, 500)], b)
-fact = [training_vals[i] for i in range(400, 500)]
-
-for i in range(len(result)):
-    if result[i] >= 0.5 and fact[i] == target_number:
-        print "%s recognized %s - %s" % (str(i), str(result[i]), str(fact[i]))
-        true_rec += 1
-    elif result[i] >= 0.5 and fact[i] != target_number:
-        print "%s false rec %s - %s" % (str(i), str(result[i]), str(fact[i]))
-        false_rec += 1
-    elif result[i] < 0.5 and fact[i] != target_number:
-        print "%s true unrec %s - %s" % (str(i), str(result[i]), str(fact[i]))
-        true_unrec += 1
-    elif result[i] < 0.5 and fact[i] == target_number:
-        print "%s unrecognized %s - %s" % (str(i), str(result[i]), str(fact[i]))
-        false_unrec += 1
-
-print "true_rec", true_rec
-print "false_rec", false_rec
-print "true_unrec", true_unrec
-print "false_unrec", false_unrec
-
+# W = init_weights(784)
+# b = 0
+# X = np.array([training_img[i] for i in range(trainig_sets)])
+# Y = np.array([normalize_val(training_vals[i], target_number) for i in range(trainig_sets)])
 #
-# print Y[17]
-# print X[17]
+# W, b = optimize(W, b, X, Y, learning_rate, num_iterations)
 #
+# true_rec, false_rec, true_unrec, false_unrec = 0, 0, 0, 0
 #
+# result = predict(W, [training_img[i] for i in range(400, 500)], b)
+# fact = [training_vals[i] for i in range(400, 500)]
 #
-# print "W upd2"
-# print W
-
-save_idealview(target_number, W)
+# for i in range(len(result)):
+#     if result[i] >= 0.5 and fact[i] == target_number:
+#         print "%s recognized %s - %s" % (str(i), str(result[i]), str(fact[i]))
+#         true_rec += 1
+#     elif result[i] >= 0.5 and fact[i] != target_number:
+#         print "%s false rec %s - %s" % (str(i), str(result[i]), str(fact[i]))
+#         false_rec += 1
+#     elif result[i] < 0.5 and fact[i] != target_number:
+#         print "%s true unrec %s - %s" % (str(i), str(result[i]), str(fact[i]))
+#         true_unrec += 1
+#     elif result[i] < 0.5 and fact[i] == target_number:
+#         print "%s unrecognized %s - %s" % (str(i), str(result[i]), str(fact[i]))
+#         false_unrec += 1
+#
+# print "true_rec", true_rec
+# print "false_rec", false_rec
+# print "true_unrec", true_unrec
+# print "false_unrec", false_unrec
 
 #save_idealview(target_number, W)
-
 
 track_end(start)
