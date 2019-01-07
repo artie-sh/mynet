268f489e614effa4d6847a63533afcd27a8b1749
artie-sh <artie.sh.87@gmail.com>
2019-01-07 23:09:42 +0100

    fixed?

--- logistic_regr_xrp.py
+++ logistic_regr_xrp.py
@@ -28,6 +28,9 @@ def get_min_max(timerow):
             maximum = line[1]
     return minimum, maximum
 
+def get_clean_timerow(timerow):
+    return np.array([line[1] for line in timerow])
+
 def normalize_vals(data, min, max):
     return (data - min) / (max - min)
 
@@ -36,24 +39,33 @@ def unwrap_prediction(prediction, minimum, maximum):
 
 start = track_start()
 
-input_size = 100
-trainig_sets = 1000
+input_size = 50
+trainig_sets = 10000
 num_iterations = 2000
-learning_rate = 0.2
+learning_rate = 0.5
 
 timerow = get_timerow(data)
 ma = get_ma(timerow, 3)
-minimum, maximum = get_min_max(timerow)
+minimum, maximum = get_min_max(ma)
 
 W = init_weights(input_size)
 b = 0
+
+
+
 X = normalize_vals(np.array([[item[1] for item in piece] for piece in [ma[i:i+input_size] for i in range(trainig_sets)]]), minimum, maximum)
+
+
+
 Y = normalize_vals(np.array([ma[i+input_size+1][1] for i in range(trainig_sets)]),  minimum, maximum)
+
 
 W, b = optimize(W, b, X, Y, learning_rate, num_iterations)
 
-vals_for_pred = normalize_vals(np.array([line[1] for line in ma[len(ma) - input_size:]]), minimum, maximum)
-vals_for_pred = vals_for_pred.reshape(100, 1)
+vals = np.array([line[1] for line in ma[len(ma) - input_size:]])
+vals_for_pred = normalize_vals(vals, minimum, maximum)
+print vals
+vals_for_pred = vals_for_pred.reshape(input_size, 1)
 
 
 prediction = predict(W, vals_for_pred.T, b)
